1e6106af4ff2f877781b0400ae5e297341a879fe
dimitrit <dimitri@tastypies.co.uk>
2019-05-21 17:53:00 +0100

    start with kid, rather than squid

--- coco/squid.asm
+++ coco/squid.asm
@@ -1,13 +1,13 @@
 you     equ     $a002
         org     $3f00
+        bra     squid
 are     ldx     #now
 a       lda     ,x+
         beq     squid
         jsr     [you]
         cmpa    #$4b
         bne     a
-        lda     ,x+
-        lda     ,x+
+        lda     ,x++
         bra     a
 squid   lda     now+9
         eora    #$18
